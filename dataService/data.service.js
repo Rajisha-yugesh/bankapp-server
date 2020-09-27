446a5c2aa28363e694e34fc59bfd30b99f0de54c
rajisha <rajishaaravindh@gmail.com>
2020-09-27 23:21:51 +0530

    mongodb

--- dataService/data.service.js
+++ dataService/data.service.js
@@ -1,9 +1,12 @@
+// const { User } = require("./db");
+const db = require("./db");
+
 accountDetails = {
-    1001: { name: "user1", acno: 1001, pin: 4387, password: "userone", balance: 3000, transactions: [] },
-    1002: { name: "user2", acno: 1002, pin: 4388, password: "usertwo", balance: 4000, transactions: [] },
-    1003: { name: "user3", acno: 1003, pin: 4287, password: "userthree", balance: 2000, transactions: [] },
-    1004: { name: "user4", acno: 1004, pin: 1388, password: "userfour", balance: 1000, transactions: [] },
-    1005: { name: "user5", acno: 1005, pin: 1318, password: "userfive", balance: 1500, transactions: [] }
+  1001: { name: "user1", acno: 1001, pin: 4387, password: "userone", balance: 3000, transactions: [] },
+  1002: { name: "user2", acno: 1002, pin: 4388, password: "usertwo", balance: 4000, transactions: [] },
+  1003: { name: "user3", acno: 1003, pin: 4287, password: "userthree", balance: 2000, transactions: [] },
+  1004: { name: "user4", acno: 1004, pin: 1388, password: "userfour", balance: 1000, transactions: [] },
+  1005: { name: "user5", acno: 1005, pin: 1318, password: "userfive", balance: 1500, transactions: [] }
 
 
 
@@ -12,154 +15,295 @@ accountDetails = {
 let currentUser;
 
 
-const register=(name, acno, pin, password)=> {
-    if (acno in accountDetails) {
+const register = (name, acno, pin, password) => {
+  return db.User.findOne({
+    acno:acno
+  })
+    .then(user => {
+
+      if (user) {
         return {
-            status: false,
-            statusCode:422,
-            message: "account already exists"
-        }
-
-    }
-
-
-
-
-    accountDetails[acno] = {
-        name,
-        acno,
-        password,
-        pin,
-        balance: 0,
-        Transactions: []
-    }
-    // console.log(this.accountDetails)
-    // this.saveDetails();
-    return {
-        status: true,
-        statusCode:200,
-        message: "account registered successfully.please login"
-    }
-}
-
-const login=(req,acno, password)=> {
-  console.log(acno,password)
-    var acno1 = parseInt(acno);
-    console.log(accountDetails);
-    var data = accountDetails;
-    if (acno1 in data) {
-      var pwd = data[acno].password
-      if (pwd == password) {
-        
-        req.session.currentUser = data[acno];
-        // this.saveDetails();
-        return{
-            status:true,
-        statusCode:200,
-            message:"login successfully"
+          status: false,
+          statusCode: 422,
+          message: "account already exists,please login"
         }
       }
+      const newUser = new db.User({
+        name,
+        acno,
+        pin,
+        password,
+        balance: 0,
+        transactions: []
+      });
+
+      newUser.save();
+
+      return {
+        status: true,
+        statusCode: 200,
+        message: "account registered successfully.please login"
+      }
+
+    })
+  // if (acno in accountDetails) {
+  //     return {
+  //         status: false,
+  //         statusCode:422,
+  //         message: "account already exists"
+  //     }
+
+  // }
+
+
+
+
+  // accountDetails[acno] = {
+  //     name,
+  //     acno,
+  //     password,
+  //     pin,
+  //     balance: 0,
+  //     Transactions: []
+  // }
+  // // console.log(this.accountDetails)
+  // // this.saveDetails();
+  // return {
+  //     status: true,
+  //     statusCode:200,
+  //     message: "account registered successfully.please login"
+  // }
+}
+
+const login = (req, acno, password) => {
+  var acno = parseInt(acno);
+
+  return db.User.findOne({
+    acno:acno,
+    
+    
+    password
+  })
+    .then(user => {
+
+      if (user) {
+        req.session.currentUser =acno;
+        return {
+          status: true,
+          statusCode: 200,
+          message: "logged in"
+        }
+      }
+      return {
+        status: false,
+        statusCode: 422,
+        message: "invalid credentials"
+      }
+    })
+
+
+      // console.log(acno, password)
+
+      // console.log(accountDetails);
+      // var data = accountDetails;
+      // if (acno1 in data) {
+      //   var pwd = data[acno].password
+      //   if (pwd == password) {
+
+      //     req.session.currentUser = data[acno];
+      //     // this.saveDetails();
+      //     return {
+      //       status: true,
+      //       statusCode: 200,
+      //       message: "login successfully"
+      //     }
+      //   }
+      // }
+      // return {
+      //   status: false,
+      //   statusCode: 422,
+      //   message: "invalid credentials"
+      // }
     }
-    return{
-        status:false,
-        statusCode:422,
-        message:"invalid credentials"
-    }
-  }
 
 const deposit = (acno, pin, amount1) => {
 
- 
-    var amount = parseInt(amount1)
-    var data = accountDetails;
-    if (acno in data) {
-        let mpin = data[acno].pin
+  return db.User.findOne({
+    acno,
+    pin,
+    amount:amount1
+  })
+    .then(user => {
 
-        if (pin == mpin) {
-            data[acno].balance += amount;
-            data[acno].transactions.push({
-
-                amount: amount,
-                type: 'credit'
-            })
-
-            // this.saveDetails();
-            return {
-                status: true,
-                statusCode: 200,
-                message: 'account has been credited',
-                balance: data[acno].balance
-            }
-
-        }
-        
-            return {
-                status: false,
-                statusCode: 422,
-                message: 'incorrect account details'
-
-            }
-        
-    }
-
-
-}
-
- const withdraw=(acno, pin, amount1)=> {
-    var amount = parseInt(amount1)
-    var data = accountDetails;
-    if (acno in data) {
-      let mpin = data[acno].pin
-
-      if(data[acno].balance < amount){
-        return{
-          status :false,
-          message :"insufficient balance",
-          balance :data[acno].balance
-        }
-      }
-
-      else if (pin == mpin) {
-        data[acno].balance -= amount;
-        data[acno].transactions.push(
-          {
-            amount:amount,
-            type:'debit'
-          }
-        )
-        // this.saveDetails();
-        return {
-          status: true,
-          statusCode :200,
-          message: 'account has been debited',
-          balance: data[acno].balance
-        }
-
-      }
-      else {
+      if (!user) {
         return {
           status: false,
-          statusCode :422,
-          message: 'incorrect account details'
+          statusCode: 422,
+          message: "incorrect account details"
+        }
+      }
 
+      user.balance+=parseInt(amount1)
+      user.transactions.push({
+        amount:amount1,
+        typeOfTransaction:'credit'
+       
+
+
+      })
+      user.save();
+  
+
+      return {
+        status: true,
+        statusCode: 200,
+        message: "account has been credited",
+        balance:user.balance
+      }
+
+    })
+
+
+    // var amount = parseInt(amount1)
+    // var data = accountDetails;
+    // if (acno in data) {
+    //   let mpin = data[acno].pin
+
+    //   if (pin == mpin) {
+    //     data[acno].balance += amount;
+    //     data[acno].transactions.push({
+
+    //       amount: amount,
+    //       type: 'credit',
+    //       id: Math.floor(Math.random() * 10000)
+    //     })
+
+    //     // this.saveDetails();
+    //     return {
+    //       status: true,
+    //       statusCode: 200,
+    //       message: 'account has been credited',
+    //       balance: data[acno].balance
+    //     }
+
+    //   }
+
+    //   return {
+    //     status: false,
+    //     statusCode: 422,
+    //     message: 'incorrect account details'
+
+    //   }
+
+    // }
+
+
+  }
+
+  const withdraw = (acno, pin, amount1) => {
+    return db.User.findOne({
+      acno,
+      pin,
+      amount:amount1
+    })
+      .then(user => {
+  
+        if (!user) {
+          return {
+            status: false,
+            statusCode: 422,
+            message: "incorrect account details"
+          }
         }
 
-      }
-    }
-
-
-  }
-
-
- const getTransactions=(req)=>{
-    return accountDetails[req.session.currentUser.acno].transactions
-  }
-
-module.exports={
-    register,
+        if(user.balance<parseInt(amount1)){
+          return{
+            status:false,
+            statusCode:422,
+            message:'insufficient balance',
+            balance:user.balance
+          }
+        }
+  
+        user.balance-=parseInt(amount1)
+        user.transactions.push({
+          amount:amount1,
+          typeOfTransaction:'debit'
+          
+  
+  
+        })
+        user.save();
     
+  
+        return {
+          status: true,
+          statusCode: 200,
+          message: "account has been debited",
+          balance:user.balance
+
+        }
+  
+      })
+
+
+  }
+
+
+  const getTransactions = (req) => {
+    return db.User.findOne({
+      acno:req.session.currentUser
+    })
+    .then(user=>{
+      if(user){
+        return user.transactions;
+      }
+    })
+    
+    // return accountDetails[req.session.currentUser.acno].transactions
+  }
+
+  const deleteTransaction = (req, id) => {
+    return db.User.findOne({
+      acno:req.session.currentUser
+    })
+    .then(user=>{
+      user.transactions = user.transactions.filter(t=>{
+        if(t._id==id){
+          return false;
+        }
+        return true
+      })
+      user.save();
+      return {
+        status: true,
+        statusCode: 200,
+        message: 'transacion delted succesfuly'
+      }
+     
+    })
+    
+    // let transactions = accountDetails[req.session.currentUser.acno].transactions;
+    // transactions = transactions.filter(t => {
+    //   if (t.id == id) {
+    //     return false;
+    //   }
+    //   return true;
+
+    // })
+    // accountDetails[req.session.currentUser.acno].transactions = transactions
+  
+
+  }
+
+
+
+  module.exports = {
+    register,
+
     login,
     deposit,
     withdraw,
     getTransactions,
-}
+    deleteTransaction,
+  }
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const dataService =require('./dataservice/data.service')
 const session =require('express-session');//we installed and required
+const cors =require('cors');
 const app =express();
+
+app.use(cors({
+    origin:'http://localhost:4200',
+    credentials :true
+}))
 
 app.use(express.json())
 
@@ -16,7 +22,7 @@ const logMiddleware=(req,res,next)=>{
     next();
 };
 
-app.use(logMiddleware);
+// app.use(logMiddleware);
 
 const authMiddleware=(req,res,next)=>{
     if(!req.session.currentUser){
@@ -33,34 +39,52 @@ const authMiddleware=(req,res,next)=>{
 
 app.post('/login',(req,res)=>{
     
-const result=dataService.login(req,req.body.acno,req.body.password)
-
-
-
-res.status(result.statusCode).json(result);
+dataService.login(req,req.body.acno,req.body.password)
+  .then(result=>{
+        res.status(result.statusCode).json(result)
+    })
 })
 
 app.post('/deposit',authMiddleware,(req,res)=>{
    
-const result=dataService.deposit(req.body.acno,req.body.pin,req.body.amount1)
-res.status(result.statusCode).json(result);
+dataService.deposit(req.body.acno,req.body.pin,req.body.amount1)
+.then(result=>{
+    res.status(result.statusCode).json(result)
+})
+
 })
 
 app.post('/register',(req,res)=>{
-    const result=dataService.register(req.body.name,req.body.acno,req.body.pin,req.body.password)
-    res.status(result.statusCode).json(result);
+    dataService.register(req.body.name,req.body.acno,req.body.pin,req.body.password)
+    .then(result=>{
+        res.status(result.statusCode).json(result)
+    })
+    // res.status(200).send("sucess")
+
+    // res.status(result.statusCode).json(result);
     })
 
 app.post('/withdraw',authMiddleware,(req,res)=>{
-const result=dataService.withdraw(req.body.acno,req.body.pin,req.body.amount1)
-res.status(result.statusCode).json(result);
+dataService.withdraw(req.body.acno,req.body.pin,req.body.amount1)
+.then(result=>{
+    res.status(result.statusCode).json(result)
+})
 })
 
 app.get('/transactions',authMiddleware,(req,res)=>{
-    const result=dataService.getTransactions(req)
-    res.status(200
-        ).json(result);
+    dataService.getTransactions(req)
+    .then(result=>{
+        res.status(result.statusCode).json(result)
     })
+
+    })
+
+app.delete('/transactions/:id', (req, res) => {
+    dataService.deleteTransaction(req, req.params.id)
+    .then(result=>{
+        res.status(result.statusCode).json(result)
+    })
+})
 
 
 
@@ -72,8 +96,8 @@ res.send("patch method"))
 app.put('/',(req,res)=>
 res.send("patch method"))
 
-app.delete('/',(req,res)=>
-res.send("patch method"))
+
+
 
 app.listen(3000,()=>{
     console.log("server started at port 3000");//it will displayed on command prompt
